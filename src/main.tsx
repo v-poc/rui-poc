b6096d97aef644b5ec100ffe3bb3b34a0ebe5bde
NikoNi <bluepspower@qq.com>
2022-04-03 16:52:19 +0800

    refactor: switch to React 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 // import React from "react";
-import { render } from "react-dom";
+// import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "rui-next/css"; // lib css
 import "./index.less"; // app styles
 
-render(
-  <App />,
-  document.getElementById("root"),
-);
+// render(
+//   <App />,
+//   document.getElementById("root"),
+// );
+const container = document.getElementById("root") as any;
+createRoot(container).render(<App />);
