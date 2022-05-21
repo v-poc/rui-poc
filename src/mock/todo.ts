59c31e157b58694459a10ccbeb30c77333c31949
NikoNi <bluepspower@qq.com>
2022-05-21 23:18:24 +0800

    refactor: add more log

--- src/mock/todo.ts
+++ src/mock/todo.ts
@@ -5,7 +5,13 @@ export default [
     url: "/api/queryTodoList",
     method: "post", // @ts-ignore
     response: ({ body, query }) => {
-      // console.log("response-body: ", body, "\nresponse-query: ", query);
+      console.log(
+        "response-body: ",
+        body,
+        "\nresponse-query: ",
+        query,
+      );
+      
       return {
         code: 0,
         message: "success",
